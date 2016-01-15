0e41260956d4af3a547bb70034123aca39c7aff9
rbsexton <robertbsexton@gmail.com>
2016-01-14 22:43:52 -0800

    Move things around

--- ridelister.py
+++ ridelister.py
@@ -3,7 +3,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
 	def get(self):
 		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.write('Toodles, World!')
+		self.response.write(' World!')
 
 app = webapp2.WSGIApplication([ ('/', MainPage), ], debug=True)
 
