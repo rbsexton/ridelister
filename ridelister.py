ee4316021bdd38cf83fa640a4c51efcf193dad09
rbsexton <robertbsexton@gmail.com>
2016-01-14 22:59:26 -0800

    Basic forms ops

--- ridelister.py
+++ ridelister.py
@@ -1,9 +1,31 @@
 import webapp2
+import cgi
+
+MAIN_PAGE_HTML = """\
+<html>
+  <body>
+      <form action="/submit" method="post">
+	<div>Ridename:<textarea name="title" rows="1" cols="60"></textarea></div>
+	<div>Starting Location: <textarea name="starting" rows="1" cols="60"></textarea></div>
+	<div>Description: <textarea name="description" rows="3" cols="60"></textarea></div>
+	<div><input type="submit" value="Submit"></div>
+      </form>
+</body>
+</html>
+"""
 
 class MainPage(webapp2.RequestHandler):
-	def get(self):
-		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.write(' World!')
+    def get(self):
+    	self.response.write(MAIN_PAGE_HTML)
 
-app = webapp2.WSGIApplication([ ('/', MainPage), ], debug=True)
+class SubmissionReport(webapp2.RequestHandler):
+    def post(self):
+	    self.response.write('<html><body>Your Ride:<pre>')
+	    self.response.write(cgi.escape(self.request.get('title')))
+	    self.response.write('</pre></body></html>')
+
+app = webapp2.WSGIApplication([
+	('/', MainPage),
+	('/submit', SubmissionReport),
+], debug=True)
 
