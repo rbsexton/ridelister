7022b4556f348215012da86b552daaf1a4da714d
rbsexton <robertbsexton@gmail.com>
2016-01-16 12:52:14 -0800

    Hook in the google auth tools for POC.

--- ridelib/rideentry.py
+++ ridelib/rideentry.py
@@ -1,18 +1,34 @@
 '''Put up the initial form for entering the ride.  Pretty basic.'''
 __all__ = (
-	"RideEntry",
+    "RideEntry",
 )
 
 import os
 import jinja2
 import webapp2
 
+from google.appengine.api import users
+
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
         extensions=['jinja2.ext.autoescape'],
-	    autoescape=True)
+        autoescape=True)
 
 class RideEntry(webapp2.RequestHandler):
     def get(self):
-		template = JINJA_ENVIRONMENT.get_template('index.html')
-		self.response.write(template.render())
+    
+        user = users.get_current_user()
+
+        if user:
+            greeting = 'Hello, ' + user.nickname()
+        else:
+            greeting = 'You are not logged into google.  Some features will be unavailable'
+        
+        template_values = {
+            'greeting': greeting,
+            }
+        
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+
+
