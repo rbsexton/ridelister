aae21a7124a97bd71c1a16afa5e513d4946672e4
Robert Sexton <robert@tilly.local>
2016-02-07 22:08:18 -0800

    Send an email on approval.  Needs a lot of work.

--- ridelib/approver.py
+++ ridelib/approver.py
@@ -157,6 +157,39 @@ class SubmissionApprove(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('submissionapprove.html')
         self.response.write(template.render(template_values))
 
+
+        # Now that we've sent the form response, send the confirmation email.
+        # There should have been a checkbox in case they didn't want it.
+
+        message = mail.EmailMessage(sender="Ride Lister <robert@kudra.com>",
+                                    subject="Your ride listing has been published")
+
+        message.body = """
+
+Thanks for submitting your ride!
+
+It has been approved and is now visible on the club calendar
+
+<p>Ride Name: %s
+<p>Ride Starting Location: %s
+<p>Ride Description: %s
+
+
+<p>If you need to cancel this ride, please do so here: <a href="cancel?dbkey=%s">Edit</a>
+<p>If you would like to re-use this ride listing, go here: <a href="resubmit?dbkey=%s">Edit</a>
+
+        """ % (
+            ridelisting.name,
+            ridelisting.startlocation,
+            ridelisting.description,
+            textkey,textkey)
+
+        if user:
+            message.to = user.email()
+            message.send()
+
+
+
 ##################################################################################
 # Here's the thing that handles the URL hit from the 
 # Google authentication system.
